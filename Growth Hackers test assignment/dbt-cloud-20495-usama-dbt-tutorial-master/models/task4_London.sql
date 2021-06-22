b96cfef926c3a27ddedcd4a66241dd15b40a68bb
Ussi94 <57776756+Ussi94@users.noreply.github.com>
2021-06-22 23:30:32 +0500

    Delete Growth Hackers test assignment/dbt-cloud-20495-usama-dbt-tutorial-master directory

--- Growth Hackers test assignment/dbt-cloud-20495-usama-dbt-tutorial-master/models/task4_London.sql
+++
@@ -1,3 +0,0 @@
-# Task 4: top cities last month
-SELECT *
-FROM `learning-317207.GH_test.task4_Lond`
--- Growth Hackers test assignment/dbt-cloud-20495-usama-dbt-tutorial-master/models/task4_NewYork.sql
+++
@@ -1,9 +0,0 @@
-# Task 4: top cities last month
-SELECT 
-    CAST(ROUND(SUM(tripduration/60)) AS numeric) AS total_ride_duration,
-    'New York' AS City
-FROM `bigquery-public-data.new_york_citibike.citibike_trips`
-WHERE starttime > (
-    SELECT DATE_SUB(DATE(MAX(starttime)),INTERVAL 1 MONTH)
-    FROM `bigquery-public-data.new_york_citibike.citibike_trips`
-    )
